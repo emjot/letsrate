1be185d90cf2c1b4ea2d6a034f0adf05b7fc1a51
Martin Salzburg <salzburg@emjot.de>
2014-02-19 00:23:18 +0100

    Move JQuery raty script into the engines asset/javascripts directory
    * Change letsrate generator to not copy the raty script into project

--- lib/generators/letsrate/letsrate_generator.rb
+++ lib/generators/letsrate/letsrate_generator.rb
@@ -1,13 +1,13 @@
 require 'rails/generators/migration'
 require 'rails/generators/active_record'
+
 class LetsrateGenerator < ActiveRecord::Generators::Base
   include Rails::Generators::Migration
 
   source_root File.expand_path('../templates', __FILE__)
 
-  desc "copying jquery.raty files to assets directory ..."
+  desc "copying jquery.raty controller and assets directory"
   def copying
-    copy_file 'jquery.raty.js', 'app/assets/javascripts/jquery.raty.js'
     copy_file 'star-on.png', 'app/assets/images/star-on.png'
     copy_file 'star-off.png', 'app/assets/images/star-off.png'
     copy_file 'star-half.png', 'app/assets/images/star-half.png'
@@ -15,7 +15,7 @@ class LetsrateGenerator < ActiveRecord::Generators::Base
     copy_file 'rater_controller.rb', 'app/controllers/rater_controller.rb'
   end
 
-  desc "model is creating..."
+  desc "create rate and rating_cache models"
   def create_model
     model_file = File.join('app/models', "#{file_path}.rb")
     raise "User model (#{model_file}) must exits." unless File.exists?(model_file)
